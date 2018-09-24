e3bb8a767eb9c97f6b9edc953f0fcd1d71e95514
eslam <eslam@yahoo.com>
2018-09-24 09:41:13 +0300

    FrameWork Done

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,60 @@ $(function () {
 
 	'use strict';
 
+	var infoH = $('.info').css('width'); // Width Of Info Div by Css
+	var infoHDy = $('info').outerWidth(true); // Width Of Info Div dynamic
+	var navH = $('nav').outerHeight(true); // Get Outer Height Of Navabr
+	
+	
+	$('.info, .content').height($(window).height()); // Adust Div Size 
+
+	$('.content').css('margin-left', infoH); // Margin-Left of Content
+
+	// Calc Nav Outer Height And Add Width To Div Sections
+
+	$('.sections, .test').height($(window).height() - navH) // Add Height To Sections
+
+	$( window ).resize(function() {
+  		$('.info, .content, .sections').height($(window).height()); // Adust Div Size
+  		infoHDy = $('info').outerWidth(true);
+  		$('.content').css('margin-left', infoHDy);
+	});
+
+	// Switching Between Sections
+	$('nav li').click(function () {
+		$(this).addClass('active').siblings().removeClass('active'); // Add Class Active To Li
+		$($(this).data('sec')).siblings().slideUp(500, function () { // FadeOut All Sibling Sections
+			$($('nav li.active').data('sec')).slideDown(500); // FadeIn Selected Section
+		});
+	});
+
+	// Close Link
+	$('.info i').click(function () {
+		$(this).hide();
+		$('.info').animate({
+			width: 0
+		},500, function () {
+  			$('.content').animate({
+  				marginLeft: 0
+  			},500, function() {
+  				$('nav i.fa-plus').show();
+  			});
+		});
+	});
+
+	// Open Link
+	$('nav i.fa-plus').click(function () {
+		$('nav i.fa-plus').hide();
+		$('.content').animate({
+  			marginLeft: infoH
+  		},500, function () {
+  			$('.info').animate({
+				width: infoH
+			},500, function () {
+				$('.info i').show();
+			});
+  		});
+	});
 
 
 });
